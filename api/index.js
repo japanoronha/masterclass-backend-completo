08e31ccc0c1e0fc2292ef60949e21b0806f11064
João Pedro Noronha <joaopdrons@gmail.com>
2020-08-12 10:28:37 -0300

    criada parte de remoçao

--- api/index.js
+++ api/index.js
@@ -1,9 +1,42 @@
 const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+
 const data = require('./urls.json');
 
+function writeFile(cb){
+  fs.writeFile(
+    path.join(__dirname, 'urls.json'),
+    JSON.stringify(data, null, 2),
+    err => {
+      if(err) throw err;
+
+      cb(JSON.stringify({message:"OK"}));
+    }
+    
+  );
+}
 
 http.createServer((req,res) => {
 
-  res.end(JSON.stringify(data));
+  const {name, url, del} = URL.parse(req.url, true).query;
+
+
+  if(!name || !url){
+    return res.end(JSON.stringify(data));
+  };
+
+  if(del){
+    data.urls = data.urls.filter(item => String(item.url) !== String(url));
+    return writeFile((message) => {
+      res.end(message);
+    });
+
+  }
+
+
+  return res.end('create');
+
 
 }).listen(3000, () => console.log('Api is running'));
